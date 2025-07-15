1030e2906e370932686377109decedebff808275
Samuel Maderson <samuel.maderson@gmail.com>
2025-07-14 23:34:06 -0300

    updated tf remote bucket

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "cwc-service-terraform-state-3nm8psib"
+    bucket         = "cwc-service-terraform-state-6trh7922"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "cwc-service-terraform-locks"
