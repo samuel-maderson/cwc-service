3bf1259cb1b5e9348fbac4cf31f1f4ca8c7fe79a
Samuel Maderson <samuel.maderson@gmail.com>
2025-07-16 22:44:38 -0300

    starting a new deploy

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "cwc-service-terraform-state-kbxewzd2"
+    bucket         = "cwc-service-terraform-state-fxlulqh1"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "cwc-service-terraform-locks"
